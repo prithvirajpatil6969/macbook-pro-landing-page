97100b7c7f2559c37e4881fd80bf5fc8cac3e301
Prithvi <prithvirajpatil6969@gmail.com>
2025-10-12 16:06:03 +0530

    Project setup done

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className='text-3xl font-bold underline text-blue-500'>Hello, Apple Macbook Pro!</h1>
+  )
+}
+
+export default App
