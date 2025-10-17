1540f562a33c6a8dad814818d234c4fe17f72768
Prithvi <prithvirajpatil6969@gmail.com>
2025-10-17 15:44:46 +0530

    minor spacing correction

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -15,7 +15,7 @@ import {noChangeParts} from "../../constants/index.js";
 import {Color, SRGBColorSpace} from 'three'
 
 export default function MacbookModel14(props) {
-    const { color } = useMacbookStore();
+  const { color } = useMacbookStore();
   const { nodes, materials, scene } = useGLTF('/models/macbook-14-transformed.glb');
 
   const texture = useTexture('/screen.png');
